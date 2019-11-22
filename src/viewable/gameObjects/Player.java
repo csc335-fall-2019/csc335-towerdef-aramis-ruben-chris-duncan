e51c2e9d44c1b2a2db8e297b03a454f4c9b21036
rtequida21 <46634102+rtequida21@users.noreply.github.com>
2019-11-22 16:41:26 -0700

    Added Market Attribute

--- src/viewable/gameObjects/Player.java
+++ src/viewable/gameObjects/Player.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import viewable.cards.*;
+import viewable.cards.abilityCards.PlunderCard;
 import viewable.cards.towers.ArcherTowerCard;
 
 public class Player {
