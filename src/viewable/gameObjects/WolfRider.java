2564d25861863208a842d07ff9f3d1184de4fa03
Duncan Rover <duncanrover@email.arizona.edu>
2019-11-22 16:03:11 -0700

    added minions

---
+++ src/viewable/gameObjects/WolfRider.java
@@ -0,0 +1,14 @@
+package viewable.gameObjects;
+
+public class WolfRider extends Minion {
+	
+	private static final int defaultHealth = 10;
+	private static final int defaultDamage = 2;
+	private static final int defaultSpeed = 2;
+
+	public WolfRider(Integer health, Integer damage, Integer speed) {
+		super(defaultHealth, defaultDamage, defaultSpeed);
+		// TODO Auto-generated constructor stub
+	}
+
+}
