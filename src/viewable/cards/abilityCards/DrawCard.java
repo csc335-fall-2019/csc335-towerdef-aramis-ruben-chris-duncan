8a5e162b6be2aee7b06f992a49d20e0900603c3e
loperjchris <loperjchris@gmail.com>
2019-11-22 16:21:43 -0700

    Added New Cards

---
+++ src/viewable/cards/abilityCards/DrawCard.java
@@ -0,0 +1,17 @@
+package viewable.cards.abilityCards;
+
+import viewable.cards.Card;
+import viewable.gameObjects.Player;
+
+public class DrawCard extends Card {
+	private static final int cardId 		= 7;
+	private static final String cardName 	= "Draw A Card";
+	private static final int cardsToDraw	= 1;
+	public DrawCard() {
+		super(cardName, cardId);
+	}
+	@Override
+	public void Ability(Player p) {
+		p.drawCards(cardsToDraw);
+	}
+}
