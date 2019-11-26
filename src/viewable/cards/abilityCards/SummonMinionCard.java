e1257d61e404a2527df75c8c4de62fc16167233c
loperjchris <loperjchris@gmail.com>
2019-11-26 15:23:54 -0700

    Added Card Images

--- src/viewable/cards/abilityCards/SummonMinionCard.java
+++ src/viewable/cards/abilityCards/SummonMinionCard.java
@@ -5,8 +5,8 @@ import viewable.gameObjects.Player;
 
 public class SummonMinionCard extends Card {
 	private static final int cardId 			= 8;
-	private static final String cardName 		= "Draw A Card";
-	private static final int mininonsToSummon	= 1;
+	private static final String cardName 		= "Summon Minions";
+	private static final int mininonsToSummon	= 2;
 	private static final int cardCost       = 1;
 	public SummonMinionCard() {
 		super(cardName, cardId, cardCost);
