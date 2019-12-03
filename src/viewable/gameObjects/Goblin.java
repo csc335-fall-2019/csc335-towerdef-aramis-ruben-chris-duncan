6641306a3ef81b347f5f5543445316bdc1964ece
Duncan Rover <duncanrover@email.arizona.edu>
2019-12-03 13:31:56 -0700

    updated getResource methods for minions

--- src/viewable/gameObjects/Goblin.java
+++ src/viewable/gameObjects/Goblin.java
@@ -1,5 +1,8 @@
 package viewable.gameObjects;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Goblin extends Minion {
 
 	private static final int defaultHealth = 5;
@@ -13,7 +16,11 @@ public class Goblin extends Minion {
 
 	@Override
 	public String getResource() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return (new File("./resources/images/tst.png")).getCanonicalPath();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			return "";
+		}
 	}
 }
